4bf1640f7c5e2aabc2f6820b147b10450e0ff006
Archives0 <evlandesg@gmail.com>
2024-09-15 17:49:35 -0500

    Project Start
    Initialize project with basic classes and basic file structure.

---
+++ Source/HollowEarth/TopDown.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TopDown.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOLLOWEARTH_API ATopDown : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/HollowEarth/TopdownController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TopdownController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOLLOWEARTH_API ATopdownController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
